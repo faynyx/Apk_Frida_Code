76bdcdc8f1f79032900960f06066e0b1b893f367
faynyx <faynyx30@gmail.com>
2026-02-19 18:54:42 +0900

    Update dynamic_dex_loading.js

--- dynamic_dex_loading.js
+++ dynamic_dex_loading.js
@@ -1,1 +1,33 @@
 
+import frida
+import time
+import sys
+import fnmatch
+
+
+
+test = 'com?' # com.~~
+
+target="com.chvi.pool" # com
+
+device = frida.get_usb_device()
+
+time.sleep(5)
+
+pid = device.spawn([target]) # Spawn
+time.sleep(5) #Without it Java.perform silently fails
+session = device.attach(pid) # Attach
+script = session.create_script(open("dynamic_dex_loading.js").read())
+script.load()
+
+print('PID: %d'%pid)
+device.resume(pid)
+
+sys.stdin.read()
+
+input()
+
+matching = [process for process in device.enumerate_processes() if fnmatch.fnmatchcase(process.name.lower(), target)]
+
+for i in matching:
+    device.kill(i.pid)
