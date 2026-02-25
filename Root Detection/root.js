4cf85d87acb19cdf7e900e90dffa381ac1193648
faynyx <faynyx30@gmail.com>
2026-02-25 18:36:46 +0900

    root

--- Root Detection/root.js
+++ Root Detection/root.js
@@ -3,47 +3,98 @@ Java.perform(function() {
     const File = Java.use("java.io.File");
     const PM = Java.use("android.app.ApplicationPackageManager");
     const Exception = Java.use("android.content.pm.PackageManager$NameNotFoundException");
+    const Runtime = Java.use("java.lang.Runtime");
+    const JString = Java.use("java.lang.String");
 
-    const package_array = ["com.topjohnwu.magisk", "eu.chainfire.supersu", "com.genymotion.superuser"]; 
-    const root_file_path = ["/system/xbin/su", "/system/bin/su", "/system/xbin/busybox", "/system/app/Superuser.apk"];
+    const root_package = ["com.topjohnwu.magisk", "eu.chainfire.supersu", "com.genymotion.superuser", "com.noshufou.android.su", "com.koushikdutta.superuser", "com.yellowes.su"]; 
+    
+    const root_path = ["/system/","/system/app/","/system/xbin/","/system/etc/","/system/bin/","/system/bin/.ext/","/system/sd/xbin/","/system/usr/we-need-root/","/data/","/data/app/","/data/local/","/data/local/bin/","/data/local/xbin/","/data/app-private/","/sbin/","/su/bin/","/dev/","/cache/"];
+    const root_file = ["su", "busybox", "magisk", "supersu", "Superuser.apk", "KingoUser.apk", "SuperSu.apk", "daemonsu"];
+
+    const guess_root_command = ["getprop", "mount", "build.prop", "id", "sh", "su"];
 
     console.log(Build.SDK_INT.value);
 
-    //test.isCheckRootingInstalled.implementation = function() {
-    //    console.log("calling ? ");
-    //};
-
-    PM.getPackageInfo.overloads.forEach(function (args) {
-        args.implementation = function () {
+    PM.getPackageInfo.overloads.forEach(function (ov) {
+        ov.implementation = function () {
             var packageName = arguments[0];
 
             console.log("[call] getPackageInfo call and guess checking root packages : ", packageName);
 
-            if (package_array.includes(packageName)) {
+            if (root_package.includes(packageName)) {
                 console.log("[bypass] bypass checking root packages : " + packageName);
 
                 throw Exception.$new(packageName); // exception
             }
 
-            return args.apply(this, arguments);
+            return ov.apply(this, arguments);
         }
     });
 
-    File.exists.implementation = function() {
+    const exists_ov = File.exists.overload();
+
+    exists_ov.implementation = function() {
         //const file = File.getName.call(this);
-        const file_path = this.getAbsolutePath();
+        //var file_path = this.getAbsolutePath();
+        for (let i=0; i < root_path.length; i++) {
+            for (let j=0; j < root_file.length; j++) {
+                var full_path = root_path[i] + root_file[j];
 
-        console.log("[call] exists call and guess checking root file : ", file_path);
+                console.log("[call] exists call and guess checking root file : ", full_path);
 
-        
-
-        if (root_file_path.includes(file_path)) {
-            console.log("[bypass] bypass checking root file : " + file_path)
-            return false;
+                if (full_path.includes(file_path)) {
+                    console.log ("[bypass] bypass checking root file : " + file_path);
+                    return false;
+                }
+            }
         }
 
-        return this.exists.call(this);
+        return exists_ov.call(this);
     }
+
+    Runtime.exec.overloads.forEach(function (ov) {
+        ov.implementation = function() { 
+            var argsType = ov.argumentTypes.map(function (t) {
+                return t.className;
+            });
+            
+            console.log(argsType);
+            console.log(argsType.length);
+            console.log(argsType[0]);
+
+            if (argsType[0] === "java.lang.String") {
+                var cmd = arguments[0]
+                console.log(cmd);
+                if (guess_root_command.some(x => cmd.includes(x))) {
+                    console.log("[call] runtime exec call");
+                    arguments[0] = "fake";
+                }
+            }
+
+            if (argsType[0] === "[Ljava.lang.String;") { 
+                var tmp = [];
+
+                for (let i=0; i < arguments[0].length; i++) {
+                    var cmd = arguments[0][i].toString();
+                    
+                    console.log(cmd);
+                    if (guess_root_command.some(x => cmd.includes(x))) {
+                        tmp.push(JString.$new("fake"));
+
+                        //arguments[0] = Java.array("java.lang.String", tmp);
+
+                        console.log("[call] runtime exec call");
+                    }
+                    else {
+                        tmp.push(JString.$new(cmd));
+                    }
+                }
+                arguments[0] = Java.array("java.lang.String", tmp);
+            }
+
+            return ov.apply(this, arguments);
+        }
+    });
 
     /*
 
@@ -71,4 +122,3 @@ Java.perform(function() {
     //    return this.getPackageInfo(packageName, flags);
     //};
 });
-
