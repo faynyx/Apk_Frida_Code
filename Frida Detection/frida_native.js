51cdd1ed48dd29af7b00b8e6c02a5172ffe56469
faynyx <faynyx30@gmail.com>
2026-02-27 18:38:45 +0900

    frida native plus

--- Frida Detection/frida_native.js
+++ Frida Detection/frida_native.js
@@ -4,13 +4,14 @@ Java.perform(function() {
 	const opendir = Module.findExportByName(null, "opendir"); 
 	const connect = Module.findExportByName(null, "connect");
     const fopen = Module.findExportByName(null, "fopen");
+	const readlinkat = Module.findExportByName(null, "readlinkat");
 
     Interceptor.attach(opendir, {
 		onEnter: function (args) {
-			this.path = Memory.readUtf8String(args[0]);
-			if (this.path.includes("/data/local/tmp")) {
+			var path = Memory.readUtf8String(args[0]);
+			if (path.indexOf("/data/local/tmp") !== -1) { 
 				console.log ("[Frida] /data/local/tmp");
-				const fake = Memory.writeUtf8String("bypass");
+				var fake = Memory.allocUtf8String("/data/local/bypass");
 				args[0] = fake;
 			}
 		},
@@ -30,44 +31,52 @@ Java.perform(function() {
 
 	Interceptor.attach(fopen, {
 		onEnter: function (args) {
-			this.path = Memory.readUtf8String(args[0]);
-			if (this.path.includes(""))
-		}
-	})
-})
+			var pid = Process.id.toString();
+			var path = Memory.readUtf8String(args[0]);
+			if (path.indexOf("/proc/" + pid + "/maps") !== -1) { // check /proc/<pid>/maps
+				var fake_maps = Memory.allocUtf8String("/proc/0/maps");
+				args[0] = fake_maps;
+			}
+			
+			if (path.indexOf("/proc/" + pid + "/task") !== -1) { // check /proc/<pid>/tasks/<tid>
+				var fake_task = Memory.allocUtf8String("/proc/0/task");
+				args[0] = fake_task;
+			}
 
-Java.perform(function() {
-
-	Interceptor.attach(Module.findExportByName(null, "fopen"), {
-		onEnter: function(args) {
-			// console.warn("[*] fopen() called !")
-			var path = Memory.readUtf8String(args[0])
-			if((path.indexOf("/proc/") !== -1) && (path.indexOf("/status") !== -1)) {
-				console.warn("[!] Debug Detection Pattern Found !")
-				Memory.writeUtf8String(args[0], "aaa")
-				// console.log("\t\t[+] new fopen : " + Memory.readUtf8String(args[0]))
+			if (path.indexOf("/proc" + pid + "/fd") !== -1) { // check /proc/<pid>/fd
+				var fake_fd = Memory.allocUtf8String("/proc/0/fd");
+				args[0] = fake_fd;
 			}
 		},
-		onLeave: function(retval) {
+		onLeave: function (retVal) {
+
 		}
 	})
-})
 
-Java.perform(function() {
-	
-	Interceptor.attach(Module.findExportByName(null, 'sprintf'), {
-		onEnter: function(args) {
-			// console.warn("[*] sprintf() called !")
-			var str = args[1].readUtf8String();
-			// console.log("\t[+] sprintf : " + str)			
-			if(str.indexOf('/proc/') !== -1 && str.indexOf('/status') !== -1) {
-				console.log("\t[+] sprintf str : " + str)
-				console.log('[+] sprintf format : ' + args[2]);
-				args[2] = ptr('0x0');
-				console.log('[!] New sprintf format : ' + args[2]);
-			}
+	// readlinkat -> 	
+	Interceptor.attach(readlinkat, {
+		onEnter: function (args) {
+			this.buf = args[2];
+			this.buf_size = args[3].toInt32();
 		},
-		onLeave: function(retval) {
+		onLeave: function(retVal) {
+			const n = retVal.toInt32();
+			if (n <= 0) return;
+
+			let out = "";
+			try {
+				out = Memory.readUtf8String(this.buf, n);
+			} catch (e) {
+				return;
+			}
+
+			if (out.indexOf("frida") !== -1) {
+				const fake = "/system/bin/linker" // not frida
+				const fakelen = Math.min(fake.length, this.buf_size);
+				
+				Memory.writeUtf8String(this.buf, fake);
+				retVal.replace(fakelen);
+			}
 		}
 	})
 })
